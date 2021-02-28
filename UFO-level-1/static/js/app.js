b47fc4a0610f45c416f2de57b4f888f1299e8dfe
Scotty P <provencher.scottucf@gmail.com>
2021-02-27 23:21:05 -0500

    display table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,24 @@
 // from data.js
-var tableData = data;
+var UFOData = data;
 
-// YOUR CODE HERE!
+// POPULATE THE INFORMATION TABLE
+
+console.log(UFOData)
+
+// Get a reference cor the table body
+var tbody = d3.select("tbody");
+
+// Loop throught tableDate and each weather report object to add the number of rows
+// based on the legth of the object
+UFOData.forEach((city) => {
+    var row = tbody.append("tr");
+    Object.entries(city).forEach(([key, value]) =>{
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// FILTER THE INFORMATION
+
+// Select the button
+
